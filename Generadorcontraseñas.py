fcf4b7e3bc385dfbe133efd85ff33b0dd80c6cb8
Mattrob-bot9 <matiasrodri2849@gmail.com>
2024-07-28 10:19:08 -0500

    Create Generadorcontraseñas.py

---
+++ Generadorcontraseñas.py
@@ -0,0 +1,7 @@
+import random
+caracteres = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+longitud_contraseña = int(input("Introduce la longitud de la contraseña: "))
+contraseña_generada = A 
+for i in range(longitud_contraseña):
+      contraseña_generada += random.choise(carateres)
+      print("la contraseña generada es: ", contraseña_generada)
